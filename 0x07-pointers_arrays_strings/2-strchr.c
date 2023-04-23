eed7157d991f225e7824509ba1feeba173a8394b
Haneenmohamed8 <125806698+Haneenmohamed8@users.noreply.github.com>
2023-04-23 22:38:52 +0200

    Create 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - Locates a character in a string.
+ * @s: The string to be searched
+ * @c: The character to be located
+ *
+ * Return: If c is found - s.
+ * If c is not found - NULL.
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+      return (s);
+		else
+      s++;
+	}
+	if (c == '\0')
+		return (s);
+
+	return (NULL);
+}
