d9bc50a8aba858605c766c07d498de181635a5a1
Haneenmohamed8 <125806698+Haneenmohamed8@users.noreply.github.com>
2023-04-27 08:16:21 +0200

    Update 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,20 @@
 section .data
-    hello db "Hello, Holberton",10,0
+    msg db "Hello, Holberton", 10, 0
 
 section .text
     global main
+
     extern printf
 
 main:
-    push hello
+    push rbp
+    mov rbp, rsp
+
+    lea rdi, [msg]
+    mov al, 0
     call printf
-    add rsp, 8
+
+    mov rsp, rbp
+    pop rbp
     xor eax, eax
     ret
