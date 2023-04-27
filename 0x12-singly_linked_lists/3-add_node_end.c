d6d72f6e7ace01a7567af5371442763952693016
Haneenmohamed8 <haneenmuhammed181@gmail.com>
2023-04-27 10:34:38 -0700

    betty

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,35 +9,35 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new_node, *last_node;
-    unsigned int len;
+	list_t *new_node, *last_node;
+	unsigned int len;
 
-    if (str == NULL)
-        return (NULL);
+	if (str == NULL)
+		return (NULL);
 
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return (NULL);
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    len = 0;
-    while (str[len] != '\0')
-        len++;
+	len = 0;
+	while (str[len] != '\0')
+		len++;
 
-    new_node->str = strdup(str);
-    new_node->len = len;
-    new_node->next = NULL;
+	new_node->str = strdup(str);
+	new_node->len = len;
+	new_node->next = NULL;
 
-    if (*head == NULL)
-    {
-        *head = new_node;
-        return (new_node);
-    }
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
 
-    last_node = *head;
-    while (last_node->next != NULL)
-        last_node = last_node->next;
+	last_node = *head;
+	while (last_node->next != NULL)
+		last_node = last_node->next;
 
-    last_node->next = new_node;
+	last_node->next = new_node;
 
-    return (new_node);
+	return (new_node);
 }
