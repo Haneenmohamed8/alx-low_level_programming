378efc99c363b15bc72a95e7b08cc7ae0ea73b55
Haneenmohamed8 <125806698+Haneenmohamed8@users.noreply.github.com>
2023-04-28 06:05:33 +0200

    Create lists.h

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,28 @@
+#ifndef LIST_H
+#define LIST_H
+
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * struct list - singly link list
+ * * @str: char arraya
+ * @len: length
+ * @next: pointer
+ */
+
+typedef struct list
+{
+	char *str;
+	unsigned int len;
+	struct list *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+list_t  *addNode(list_t **head_ref, const char *new_data);
+void free_list(list_t *head);
+
+#endif 
