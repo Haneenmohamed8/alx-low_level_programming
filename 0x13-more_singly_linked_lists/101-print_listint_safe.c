23b059b02577dbe2fed84a0ebceaa5224c95716e
Haneenmohamed8 <125806698+Haneenmohamed8@users.noreply.github.com>
2023-04-30 06:38:41 +0300

    Update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,10 +1,9 @@
-#include "lists.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
+#include <stdio.h>
+#include "lists.h"
 
 /**
- * print_listint_safe - prints a listint_t linked list safely
+ * print_listint_safe - prints a listint_t linked list
  * @head: pointer to the head of the list
  *
  * Return: the number of nodes in the list
@@ -13,34 +12,26 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t count = 0;
-	const listint_t *node = head;
-	const size_t table_size = 1024;
-	bool visited[table_size];
-	size_t i;
-	
-	for (i = 0; i < table_size; i++)
-		visited[i] = false;
+	const listint_t *current, *prev;
 
-	while (node != NULL) 
+	if (!head)
+		exit(98);
+
+	current = head;
+
+	while (current)
 	{
-		printf("[%p] %d\n", (void *)node, node->n);
 		count++;
+		printf("[%p] %d\n", (void *) current, current->n);
+		prev = current;
+		current = current->next;
 
-	/* Check if the next node has already been visited */
-	if (node->next != NULL && visited[(size_t)node->next % table_size]) 
-	{
-		printf("-> [%p] %d\n", (void *)node->next, node->next->n);
-		break;
+		if (prev <= current)
+			break;
 	}
 
-		visited[(size_t)node % table_size] = true;
-		node = node->next;
-	}
+	if (current)
+		printf("-> [%p] %d\n", (void *) current, current->n);
 
-	    /* If we reached the end of the list, return the number of nodes */
-	if (node == NULL)
-		return (count);
-
-	/* If we found a cycle, exit the program with status 98 */
-	exit(98);
+	return (count);
 }
