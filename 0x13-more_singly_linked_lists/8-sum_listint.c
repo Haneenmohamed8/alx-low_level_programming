defef3cec85a6e51d59b944334099bba8e72699e
Haneenmohamed8 <125806698+Haneenmohamed8@users.noreply.github.com>
2023-04-30 06:21:37 +0300

    Update 8-sum_listint.c

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -12,12 +12,12 @@ int sum_listint(listint_t *head)
 	int sum = 0;
 	listint_t *current = head;
 
-	while (current != NULL)
+	while(current != NULL)
 	{
 		sum += current->n;
 		current = current->next;
 	}
 
-	return sum;
+	return (sum);
 }
 
