e74c3aea242d36912d15d8e334ca1f881f305fdf
Haneenmohamed8 <125806698+Haneenmohamed8@users.noreply.github.com>
2023-04-30 06:20:23 +0300

    Update 8-sum_listint.c

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -6,17 +6,18 @@
  *
  * Return: sum of all the data (n) or 0 if the list is empty
  */
+
 int sum_listint(listint_t *head)
 {
-    int sum = 0;
-    listint_t *current = head;
+	int sum = 0;
+	listint_t *current = head;
 
-    while (current != NULL)
-    {
-        sum += current->n;
-        current = current->next;
-    }
+	while (current != NULL)
+	{
+		sum += current->n;
+		current = current->next;
+	}
 
-    return sum;
+	return sum;
 }
 
