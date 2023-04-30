e55bb3111b0437abe3c8324e3d68b138359dcb7a
Haneenmohamed8 <125806698+Haneenmohamed8@users.noreply.github.com>
2023-04-30 05:48:15 +0300

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -6,15 +6,16 @@
  *
  * Return: number of elements in the list
  */
+
 size_t listint_len(const listint_t *h)
 {
-    size_t len = 0;
+	size_t len = 0;
 
-    while (h != NULL)
-    {
-        len++;
-        h = h->next;
-    }
+	while (h != NULL)
+	{
+		len++;
+		h = h->next;
+	}
 
-    return (len);
+	return (len);
 }
