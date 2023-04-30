c6d1003c3390eb1bcdbdde6553fdb85ad651cb4e
Haneenmohamed8 <125806698+Haneenmohamed8@users.noreply.github.com>
2023-04-30 03:21:47 +0300

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,17 +6,17 @@
  *
  * Return: the number of nodes
  */
- 
+
 size_t print_listint(const listint_t *h)
 {
-    size_t nodes = 0;
+      size_t nodes = 0;
 
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        nodes++;
-    }
+	     while (h != NULL)
+	    {
+		printf("%d\n", h->n);
+		h = h->next;
+		nodes++;
+	    }
 
-    return (nodes);
+	    return (nodes);
 }
