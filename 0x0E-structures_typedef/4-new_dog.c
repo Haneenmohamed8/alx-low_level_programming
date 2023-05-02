eb3a302afaff774980591409dc94484c686629a5
Haneenmohamed8 <125806698+Haneenmohamed8@users.noreply.github.com>
2023-05-02 23:33:52 +0300

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -26,12 +26,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	name_len++;
 
 	new_dog->name = malloc(name_len * sizeof(char));
-	if (new_dog->name == NULL)
-	{
+	if (new_dog->name == NULL){
 		free(new_dog);
 		return (NULL);
 	}
-
 	for (i = 0; i < name_len; i++)
 		new_dog->name[i] = name[i];
 
@@ -42,13 +40,11 @@ dog_t *new_dog(char *name, float age, char *owner)
 	owner_len++;
 
 	new_dog->owner = malloc(owner_len * sizeof(char));
-	if (new_dog->owner == NULL)
-	{
+	if (new_dog->owner == NULL){
 		free(new_dog->name);
 		free(new_dog);
 		return (NULL);
 	}
-
 	for (i = 0; i < owner_len; i++)
 		new_dog->owner[i] = owner[i];
 
