3ffb51576c3944eb37bb7a2df7056d7b0bd085b5
Haneenmohamed8 <125806698+Haneenmohamed8@users.noreply.github.com>
2023-05-03 00:53:44 +0300

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -15,18 +15,15 @@ int main(int argc, char *argv[])
 	int i, j, len1, len2, len;
 	int *result;
 	char *num1, *num2;
-
 	if (argc != 3)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
 	num1 = argv[1];
 	num2 = argv[2];
 	len1 = strlen(num1);
 	len2 = strlen(num2);
-
 	for (i = 0; i < len1; i++)
 	{
 		if (num1[i] < '0' || num1[i] > '9')
@@ -35,7 +32,6 @@ int main(int argc, char *argv[])
 			exit(98);
 		}
 	}
-
 	for (i = 0; i < len2; i++)
 	{
 		if (num2[i] < '0' || num2[i] > '9')
@@ -44,16 +40,13 @@ int main(int argc, char *argv[])
 			exit(98);
 		}
 	}
-
 	len = len1 + len2;
 	result = calloc(len, sizeof(int));
-
 	if (result == NULL)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
 	for (i = len1 - 1; i >= 0; i--)
 	{
 		for (j = len2 - 1; j >= 0; j--)
@@ -63,7 +56,6 @@ int main(int argc, char *argv[])
 			result[i + j + 1] %= 10;
 		}
 	}
-
 	i = 0;
 	while (result[i] == 0 && i < len - 1)
 		i++;
@@ -73,6 +65,5 @@ int main(int argc, char *argv[])
 
 	printf("\n");
 	free(result);
-
 	return (0);
 }
