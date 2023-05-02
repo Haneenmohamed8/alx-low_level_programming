068737753810f38fe4013036c477defe22d6f371
Haneenmohamed8 <125806698+Haneenmohamed8@users.noreply.github.com>
2023-05-03 00:49:23 +0300

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -13,66 +13,66 @@
 int main(int argc, char *argv[])
 {
 	int i, j, len1, len2, len;
-    int *result;
-    char *num1, *num2;
+	int *result;
+	char *num1, *num2;
 
-    if (argc != 3)
-    {
-        printf("Error\n");
-        exit(98);
-    }
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-    num1 = argv[1];
-    num2 = argv[2];
-    len1 = strlen(num1);
-    len2 = strlen(num2);
+	num1 = argv[1];
+	num2 = argv[2];
+	len1 = strlen(num1);
+	len2 = strlen(num2);
 
-    for (i = 0; i < len1; i++)
-    {
-        if (num1[i] < '0' || num1[i] > '9')
-        {
-            printf("Error\n");
-            exit(98);
-        }
-    }
+	for (i = 0; i < len1; i++)
+	{
+		if (num1[i] < '0' || num1[i] > '9')
+		{
+			printf("Error\n");
+			exit(98);
+		}
+	}
 
-    for (i = 0; i < len2; i++)
-    {
-        if (num2[i] < '0' || num2[i] > '9')
-        {
-            printf("Error\n");
-            exit(98);
-        }
-    }
+	for (i = 0; i < len2; i++)
+	{
+		if (num2[i] < '0' || num2[i] > '9')
+		{
+			printf("Error\n");
+			exit(98);
+		}
+	}
 
-    len = len1 + len2;
-    result = calloc(len, sizeof(int));
+	len = len1 + len2;
+	result = calloc(len, sizeof(int));
 
-    if (result == NULL)
-    {
-        printf("Error\n");
-        exit(98);
-    }
+	if (result == NULL)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-    for (i = len1 - 1; i >= 0; i--)
-    {
-        for (j = len2 - 1; j >= 0; j--)
-        {
-            result[i + j + 1] += (num1[i] - '0') * (num2[j] - '0');
-            result[i + j] += result[i + j + 1] / 10;
-            result[i + j + 1] %= 10;
-        }
-    }
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			result[i + j + 1] += (num1[i] - '0') * (num2[j] - '0');
+			result[i + j] += result[i + j + 1] / 10;
+			result[i + j + 1] %= 10;
+		}
+	}
 
-    i = 0;
-    while (result[i] == 0 && i < len - 1)
-        i++;
+	i = 0;
+	while (result[i] == 0 && i < len - 1)
+		i++;
 
-    while (i < len)
-        printf("%d", result[i++]);
+	while (i < len)
+		printf("%d", result[i++]);
 
-    printf("\n");
-    free(result);
+	printf("\n");
+	free(result);
 
 	return (0);
 }
