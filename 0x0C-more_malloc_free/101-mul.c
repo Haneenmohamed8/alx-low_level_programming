5f39a36dabb891a8223507239d83fa3524652e32
Haneenmohamed8 <125806698+Haneenmohamed8@users.noreply.github.com>
2023-05-03 01:03:39 +0300

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+int check_digits(char *num);
+void multiply(char *num1, char *num2, int len1, int len2);
 
 /**
  * main - multiplies two positive numbers
@@ -12,34 +16,44 @@
 
 int main(int argc, char *argv[])
 {
-	int i, j, len1, len2, len;
-	int *result;
-	char *num1, *num2;
-	if (argc != 3)
+	if (argc != 3 || check_digits(argv[1]) || check_digits(argv[2]))
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	num1 = argv[1];
-	num2 = argv[2];
-	len1 = strlen(num1);
-	len2 = strlen(num2);
-	for (i = 0; i < len1; i++)
+	multiply(argv[1], argv[2], strlen(argv[1]), strlen(argv[2]));
+	return (0);
+}
+
+/**
+ * check_digits - checks if string contains only digits
+ * @num: the string to check
+ *
+ * Return: 0 if string contains only digits, 1 otherwise.
+ */
+
+int check_digits(char *num)
+{
+	while (*num)
 	{
-		if (num1[i] < '0' || num1[i] > '9')
-		{
-			printf("Error\n");
-			exit(98);
-		}
+		if (*num < '0' || *num > '9')
+			return (1);
+		num++;
 	}
-	for (i = 0; i < len2; i++)
-	{
-		if (num2[i] < '0' || num2[i] > '9')
-		{
-			printf("Error\n");
-			exit(98);
-		}
-	}
+	return (0);
+}
+
+/**
+ * multiply - multiplies two positive numbers
+ * @num1: the first number
+ * @num2: the second number
+ * @len1: the length of the first number
+ * @len2: the length of the second number
+ */
+
+void multiply(char *num1, char *num2, int len1, int len2)
+{
+	int i, j, len, *result;
 	len = len1 + len2;
 	result = calloc(len, sizeof(int));
 	if (result == NULL)
@@ -59,11 +73,9 @@ int main(int argc, char *argv[])
 	i = 0;
 	while (result[i] == 0 && i < len - 1)
 		i++;
-
 	while (i < len)
 		printf("%d", result[i++]);
 
 	printf("\n");
 	free(result);
-	return (0);
 }
