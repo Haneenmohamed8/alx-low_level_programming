0ed91d78688505824360f80cf2e6bc5d768a2d5c
Haneenmohamed8 <125806698+Haneenmohamed8@users.noreply.github.com>
2023-05-02 23:21:19 +0300

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
 
@@ -7,45 +8,46 @@
  * @age: age of the dog
  * @owner: owner of the dog
  *
- * Return: pointer to the new dog, or NULL if it fails
+ * Return: a pointer to the new dog, or NULL on failure
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
-	char *new_name, *new_owner;
-	int name_len, owner_len;
+    dog_t *new_dog;
+    char *name_copy, *owner_copy;
+    int name_len, owner_len;
 
-    /* compute the length of the name and owner strings */
-	for (name_len = 0; name[name_len] != '\0'; name_len++)
-		;
-	for (owner_len = 0; owner[owner_len] != '\0'; owner_len++)
-		;
+    /* Calculate length of name and owner strings */
+    for (name_len = 0; name[name_len]; name_len++)
+        ;
+    for (owner_len = 0; owner[owner_len]; owner_len++)
+        ;
 
-    /* allocate memory for the new dog struct and the name/owner strings */
-	new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL)
-		return (NULL);
-	new_name = malloc((name_len + 1) * sizeof(char));
-	if (new_name == NULL) {
-		free(new_dog);
-		return (NULL);
-	}
-	new_owner = malloc((owner_len + 1) * sizeof(char));
-	if (new_owner == NULL) {
-		free(new_name);
-		free(new_dog);
-		return (NULL);
-	}
+    /* Allocate memory for new dog and copied strings */
+    new_dog = malloc(sizeof(dog_t));
+    if (new_dog == NULL)
+        return (NULL);
+    name_copy = malloc(sizeof(char) * (name_len + 1));
+    if (name_copy == NULL) {
+        free(new_dog);
+        return (NULL);
+    }
+    owner_copy = malloc(sizeof(char) * (owner_len + 1));
+    if (owner_copy == NULL) {
+        free(new_dog);
+        free(name_copy);
+        return (NULL);
+    }
 
-    /* copy the name and owner strings to the new memory locations */
-	for (int i = 0; i <= name_len; i++)
-		new_name[i] = name[i];
-	for (int i = 0; i <= owner_len; i++)
-		new_owner[i] = owner[i];
+    /* Copy name and owner strings */
+    for (int i = 0; i <= name_len; i++)
+        name_copy[i] = name[i];
+    for (int i = 0; i <= owner_len; i++)
+        owner_copy[i] = owner[i];
 
-    /* initialize the new dog struct and return it */
-	new_dog->name = new_name;
-	new_dog->age = age;
-	new_dog->owner = new_owner;
-	return (new_dog);
+    /* Initialize new dog */
+    new_dog->name = name_copy;
+    new_dog->age = age;
+    new_dog->owner = owner_copy;
+
+    return (new_dog);
 }
