4f8ab066665eb2f922ddadc5ed3f9506893bd69c
Haneenmohamed8 <125806698+Haneenmohamed8@users.noreply.github.com>
2023-05-02 20:25:46 +0300

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -10,10 +10,6 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
-
-#include <stdarg.h>
 
 typedef struct print_fmt
 {
