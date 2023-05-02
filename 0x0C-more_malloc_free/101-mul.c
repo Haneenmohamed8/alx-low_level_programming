f92f80f6dd8078ebbfb56ab754ec6b99bf656a2d
Haneenmohamed8 <125806698+Haneenmohamed8@users.noreply.github.com>
2023-05-03 01:09:27 +0300

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -54,8 +54,8 @@ int check_digits(char *num)
 void multiply(char *num1, char *num2, int len1, int len2)
 {
 	int i, j, len, *result;
-	len = len1 + len2;
 
+	len = len1 + len2;
 	result = calloc(len, sizeof(int));
 
 	if (result == NULL)
