f46b0166d94c78984997ea04fa0b2ea892cc511e
Haneenmohamed8 <125806698+Haneenmohamed8@users.noreply.github.com>
2023-05-04 13:38:26 +0300

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,21 +9,21 @@
  */
 int wildcmp_helper(char *s1, char *s2)
 {
-    if (*s1 == '\0' && *s2 == '\0') /* base case */
-        return (1);
+	if (*s1 == '\0' && *s2 == '\0') /* base case */
+		return (1);
 
-    if (*s2 == '*') /* if s2 contains '*', recursively check both sides of '*' */
-    {
-        if (*s1 == '\0') /* if s1 is empty, move s2 to the end of the string */
-            return (wildcmp_helper(s1, s2 + 1));
+	if (*s2 == '*') /* if s2 contains '*', recursively check both sides of '*' */
+	{
+		if (*s1 == '\0') /* if s1 is empty, move s2 to the end of the string */
+			return (wildcmp_helper(s1, s2 + 1));
 
-        return (wildcmp_helper(s1 + 1, s2) || wildcmp_helper(s1, s2 + 1));
-    }
+		return (wildcmp_helper(s1 + 1, s2) || wildcmp_helper(s1, s2 + 1));
+	}
 
-    if (*s1 == *s2) /* if the characters match, continue checking the rest of the string */
-        return (wildcmp_helper(s1 + 1, s2 + 1));
+	if (*s1 == *s2) /* if the characters match, continue checking the rest of the string */
+		return (wildcmp_helper(s1 + 1, s2 + 1));
 
-    return (0); /* if none of the conditions match, return 0 */
+	return (0); /* if none of the conditions match, return 0 */
 }
 
 /**
@@ -36,8 +36,8 @@ int wildcmp_helper(char *s1, char *s2)
  */
 int wildcmp(char *s1, char *s2)
 {
-    if (*s2 == '*' && *(s2 + 1) == '\0') /* if s2 only contains '*', it matches any string */
-        return (1);
+	if (*s2 == '*' && *(s2 + 1) == '\0') /* if s2 only contains '*', it matches any string */
+		return (1);
 
-    return (wildcmp_helper(s1, s2)); /* call the helper function to compare the strings */
+	return (wildcmp_helper(s1, s2)); /* call the helper function to compare the strings */
 }
