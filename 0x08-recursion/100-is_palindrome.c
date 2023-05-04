ce651b761e02d15ef20d5964ffc5f9d2f7e65725
Haneenmohamed8 <125806698+Haneenmohamed8@users.noreply.github.com>
2023-05-04 13:43:48 +0300

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -13,20 +13,6 @@ int _strlen_recursion(char *s)
     return (1 + _strlen_recursion(s + 1));
 }
 
-/**
- * is_palindrome - Checks if a string is a palindrome
- * @s: The string to check
- *
- * Return: 1 if @s is a palindrome, 0 otherwise
- */
-int is_palindrome(char *s)
-{
-    int len = _strlen_recursion(s);
-
-    if (len <= 1)
-        return (1);
-    return (_compare_chars(s, s + len - 1));
-}
 
 /**
  * _compare_chars - Compares two chars recursively
@@ -42,4 +28,20 @@ int _compare_chars(char *start, char *end)
     if (start >= end)
         return (1);
     return (_compare_chars(start + 1, end - 1));
+}
+
+
+/**
+ * is_palindrome - Checks if a string is a palindrome
+ * @s: The string to check
+ *
+ * Return: 1 if @s is a palindrome, 0 otherwise
+ */
+int is_palindrome(char *s)
+{
+    int len = _strlen_recursion(s);
+
+    if (len <= 1)
+        return (1);
+    return (_compare_chars(s, s + len - 1));
 }
