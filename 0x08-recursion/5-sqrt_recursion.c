87f5ae601fbf4d58a1e0dc4568c635026edb3cb9
Haneenmohamed8 <125806698+Haneenmohamed8@users.noreply.github.com>
2023-05-04 13:22:30 +0300

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,44 +2,32 @@
 #include <stdio.h>
 
 /**
- * _sqrt_recursion - returns the natural square root of a number
- * @n: the number to calculate the square root of
+ * find_sqrt - finds the square root recursively
+ * @n: number to find the square root of
+ * @sqrt: current square root candidate
  *
- * Return: the square root of n, or -1 if n does not have a natural square root
+ * Return: square root of n or -1 if n doesn't have a natural square root
+ */
+int find_sqrt(int n, int sqrt)
+{
+	if (sqrt * sqrt > n)
+		return (-1);
+	if (sqrt * sqrt == n)
+		return (sqrt);
+
+	return (find_sqrt(n, sqrt + 1));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to find the square root of
+ *
+ * Return: square root of n or -1 if n doesn't have a natural square root
  */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
 
-	if (n == 0 || n == 1)
-		return (n);
-
-	return (sqrt_recursion_helper(n, 1, n));
-}
-
-/**
- * sqrt_recursion_helper - recursively calculates the square root of n
- * @n: the number to calculate the square root of
- * @low: the lowest possible value of the square root
- * @high: the highest possible value of the square root
- *
- * Return: the square root of n, or -1 if n does not have a natural square root
- */
-int sqrt_recursion_helper(int n, int low, int high)
-{
-	int mid;
-
-	if (low > high)
-		return (-1);
-
-	mid = (low + high) / 2;
-
-	if (mid * mid == n)
-		return (mid);
-
-	if (mid * mid > n)
-		return (sqrt_recursion_helper(n, low, mid - 1));
-
-	return (sqrt_recursion_helper(n, mid + 1, high));
+	return (find_sqrt(n, 0));
 }
