11ceea3c4de58764543335d07225f458fc1d800a
Haneenmohamed8 <125806698+Haneenmohamed8@users.noreply.github.com>
2023-05-04 13:54:02 +0300

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,35 +9,35 @@
  */
 int wildcmp_helper(char *s1, char *s2)
 {
-	if (*s1 == '\0' && *s2 == '\0') /* base case */
+	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
 
-	if (*s2 == '*') /* if s2 contains '*', recursively check both sides of '*' */
+	if (*s2 == '*')
 	{
-		if (*s1 == '\0') /* if s1 is empty, move s2 to the end of the string */
+		if (*s1 == '\0')
 			return (wildcmp_helper(s1, s2 + 1));
 
 		return (wildcmp_helper(s1 + 1, s2) || wildcmp_helper(s1, s2 + 1));
 	}
 
-	if (*s1 == *s2) /* if the characters match, continue checking the rest of the string */
+	if (*s1 == *s2)
 		return (wildcmp_helper(s1 + 1, s2 + 1));
 
-	return (0); /* if none of the conditions match, return 0 */
+	return (0);
 }
 
 /**
- * wildcmp - compares two strings and returns 1 if the strings can be considered
+ * wildcmp - compares 2 strings and returns 1 if strings can be considered
  * identical, otherwise return 0.
  * @s1: string 1
  * @s2: string 2
  *
- * Return: 1 if the strings can be considered identical, otherwise return 0
+ * Return: 1 if the strings can be considered identical,otherwise return 0
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s2 == '*' && *(s2 + 1) == '\0') /* if s2 only contains '*', it matches any string */
+	if (*s2 == '*' && *(s2 + 1) == '\0')
 		return (1);
 
-	return (wildcmp_helper(s1, s2)); /* call the helper function to compare the strings */
+	return (wildcmp_helper(s1, s2));
 }
