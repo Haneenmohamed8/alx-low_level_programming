0ba4d2e719e393a0472cc8a5c31e2a1457e8dc1d
Haneenmohamed8 <125806698+Haneenmohamed8@users.noreply.github.com>
2023-05-08 23:07:03 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,40 +9,40 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    ssize_t bytes_read;
-    int file_descriptor;
-    char *buffer;
+	ssize_t bytes_read;
+	int file_descriptor;
+	char *buffer;
 
-    if (filename == NULL)
-        return (0);
+	if (filename == NULL)
+		return (0);
 
-    buffer = malloc(sizeof(char) * letters);
-    if (buffer == NULL)
-        return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
 
-    file_descriptor = open(filename, O_RDONLY);
-    if (file_descriptor == -1)
-    {
-        free(buffer);
-        return (0);
-    }
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
+	{
+		free(buffer);
+		return (0);
+	}
 
-    bytes_read = read(file_descriptor, buffer, letters);
-    if (bytes_read == -1)
-    {
-        free(buffer);
-        close(file_descriptor);
-        return (0);
-    }
+	bytes_read = read(file_descriptor, buffer, letters);
+	if (bytes_read == -1)
+	{
+		free(buffer);
+		close(file_descriptor);
+		return (0);
+	}
 
-    if (write(STDOUT_FILENO, buffer, bytes_read) != bytes_read)
-    {
-        free(buffer);
-        close(file_descriptor);
-        return (0);
-    }
+	if (write(STDOUT_FILENO, buffer, bytes_read) != bytes_read)
+	{
+		free(buffer);
+		close(file_descriptor);
+		return (0);
+	}
 
-    free(buffer);
-    close(file_descriptor);
-    return (bytes_read);
+	free(buffer);
+	close(file_descriptor);
+	return (bytes_read);
 }
