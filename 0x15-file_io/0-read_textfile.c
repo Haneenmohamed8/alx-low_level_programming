d8fb89f0c479101124d66c99cc32fbcd502fe138
Haneenmohamed8 <125806698+Haneenmohamed8@users.noreply.github.com>
2023-05-08 21:58:39 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,52 +1,48 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- * @filename: name of the file to read
- * @letters: number of letters it should read and print
+ * read_textfile - reads a text file and prints it to POSIX stdout
+ * @filename: the name of the file to read
+ * @letters: the number of letters to read from the file
  *
- * Return: actual number of letters it could read and print, or 0 on failure
+ * Return: the actual number of letters it could read and print, or 0 on failure
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t n_bytes_read, n_bytes_written;
-	char *buffer;
+    ssize_t bytes_read;
+    int file_descriptor;
+    char *buffer;
 
-	if (filename == NULL)
-		return (0);
+    if (filename == NULL)
+        return (0);
 
-	buffer = malloc(sizeof(char) * (letters + 1));
-	if (buffer == NULL)
-		return (0);
+    buffer = malloc(sizeof(char) * letters);
+    if (buffer == NULL)
+        return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-	{
-		free(buffer);
-		return (0);
-	}
+    file_descriptor = open(filename, O_RDONLY);
+    if (file_descriptor == -1)
+    {
+        free(buffer);
+        return (0);
+    }
 
-	n_bytes_read = read(fd, buffer, letters);
-	if (n_bytes_read == -1)
-	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
+    bytes_read = read(file_descriptor, buffer, letters);
+    if (bytes_read == -1)
+    {
+        free(buffer);
+        close(file_descriptor);
+        return (0);
+    }
 
-	buffer[n_bytes_read] = '\0';
+    if (write(STDOUT_FILENO, buffer, bytes_read) != bytes_read)
+    {
+        free(buffer);
+        close(file_descriptor);
+        return (0);
+    }
 
-	n_bytes_written = write(STDOUT_FILENO, buffer, n_bytes_read);
-	if (n_bytes_written == -1 || n_bytes_written != n_bytes_read)
-	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
-
-	free(buffer);
-	close(fd);
-
-	return (n_bytes_written);
+    free(buffer);
+    close(file_descriptor);
+    return (bytes_read);
 }
